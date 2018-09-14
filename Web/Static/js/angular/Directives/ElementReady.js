638bd3474fc194b61af81a98ca59129877b8b329
Karl Elmberg <karl.elmberg@addpro.se>
2018-09-14 17:19:07 +0200

    Rss + search

---
+++ Web/Static/js/angular/Directives/ElementReady.js
@@ -0,0 +1,26 @@
+﻿app.directive('onFinishRender', ['$timeout', '$parse', function ($timeout, $parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit('ngRepeatFinished');
+                    if (!!attr.onFinishRender) {
+                        $parse(attr.onFinishRender)(scope);
+                    }
+                });
+            }
+
+            if (!!attr.onStartRender) {
+                if (scope.$first === true) {
+                    $timeout(function () {
+                        scope.$emit('ngRepeatStarted');
+                        if (!!attr.onStartRender) {
+                            $parse(attr.onStartRender)(scope);
+                        }
+                    });
+                }
+            }
+        }
+    }
+}]);
---
+++ Web/obj/Release/Package/PackageTmp/Static/js/site.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+
+    $(".header .main-header .container .log-in-search .search").click(function () {
+        $(".header .log-in-panel").css(display, "block");
+        $(".header .log-in-panel").slideDown("slow");
+        });
+});
